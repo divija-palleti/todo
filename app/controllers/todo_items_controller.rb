ba12bce83d23b70cef1945954b4031c372015c3e
Divija Palleti <IHM2016005@iiita.ac.in>
2020-05-19 09:19:57 +0530

    Added priority and due date

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -31,6 +31,6 @@ class TodoItemsController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
    end
    def todo_item_params
-    params[:todo_item].permit(:content)
+    params[:todo_item].permit(:content, :priority, :duedate)
    end
    end
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,6 +1,17 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+
+
+
+
+
+
+
+
+  
+  enum priority: [:high, :medium, :low]
   def completed?
     !completed_at.blank?
    end
+   validates :content, presence: true
 end
