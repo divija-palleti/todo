45e1eb0003eb3820cf133a2704e656914ab9d880
Divija Palleti <IHM2016005@iiita.ac.in>
2020-05-19 13:58:57 +0530

    Validation errors

--- db/migrate/20200518121043_create_todo_items.rb
+++ db/migrate/20200518121043_create_todo_items.rb
@@ -1,7 +1,7 @@
 class CreateTodoItems < ActiveRecord::Migration[6.0]
   def change
     create_table :todo_items do |t|
-      t.string :content, default: "Title-1"
+      t.string :content
       t.references :todo_list, null: false, foreign_key: true
 
       t.timestamps
